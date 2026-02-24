298623acaff6d5dc925122fcafa630acc91e98f7
unknown <nsvrindia@gmail.com>
2026-02-24 16:07:52 +0530

    java script

--- script.js
+++ script.js
@@ -4,4 +4,32 @@ var num = 10 ;
 console.log(num) ;
 console.log (typeof(num)) ;
 
+let greeting = "hello, world !" ;
+console.log(greeting ) ;
+let name;
+console.log(name);
+
+var person ={
+    name : 'rishabh goel nair' ,
+    age : '20' ,
+    title : 'student' ,
+
+}
+const syml = Symbol('symbol') ;
+console.log(syml) ;
+const sym2 = Symbol('symbol') ;
+console.log(sym2) ;
+const a = 6 , b = 7 ;
+console.log(a == 6 && b == 7) ;
+console.log(a == 6 || b == 8) ;
+
+// for loop
+for (let i = 0 ; i < 5 ; i++){
+    console.log(i) ;
+}
+var counter ;
+for (counter = 0 ; counter < 5 ; counter++){
+    document.write("current counter value is : " + counter + "<br>");
+    console.log(counter) ;
+}
 
